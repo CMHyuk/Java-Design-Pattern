ffefcb4f432561cf663b49001444ba04862aade4
CMHyuk <alsgur9710@naver.com>
2022-10-12 23:19:40 +0900

    state pattern

---
+++ state/ON.java
@@ -0,0 +1,19 @@
+package state;
+
+public class ON implements State {
+
+    public ON(Light light) {
+        light.setState(this);
+    }
+
+    @Override
+    public void on_button_pushed(Light light) {
+        System.out.println("반응 없음");
+    }
+
+    @Override
+    public void off_button_pushed(Light light) {
+        System.out.println("Light Off!");
+        light.setState(new OFF(light));
+    }
+}
