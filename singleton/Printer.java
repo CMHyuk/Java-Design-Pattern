1a6cfa58dc33238c220e17c11f88b68b00abc7e6
CMHyuk <alsgur9710@naver.com>
2022-10-16 21:22:53 +0900

    singleton pattern

---
+++ singleton/Printer.java
@@ -0,0 +1,23 @@
+package singleton;
+
+public class Printer {
+    private static Printer printer = null;
+
+    private Printer() {}
+
+    public synchronized static Printer getPrinter() {
+        if (printer == null) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+
+            }
+            printer = new Printer();
+        }
+        return printer;
+    }
+
+    public void print(String str) {
+        System.out.println(str);
+    }
+}
